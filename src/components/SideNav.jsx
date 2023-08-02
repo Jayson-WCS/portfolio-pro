7c9b2f26ea32d29988c57ecaecb414edce728316
Jayson_Delion <jayson-delion_student2023@wilder.school>
2023-08-02 23:42:04 +0200

    quick fixes

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -13,19 +13,19 @@ export const SideNav = () => {
       <AiOutlineMenu onClick={handleNav} className="absolute top-4 right-4 z-[99] md:hidden"/>
       { nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
-          <a href="#main" className="w-[75%] flex justify-center items-center rounded-full drop-shadow-lg bg-gray-100 drop-shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in-out duration-200">
+          <a onClick={handleNav} href="#main" className="w-[75%] flex justify-center items-center rounded-full drop-shadow-lg bg-gray-100 drop-shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in-out duration-200">
             <AiOutlineHome size={20}/>
             <span className="pl-4">Accueil</span>
           </a>
-          <a href="#work" className="w-[75%] flex justify-center items-center rounded-full drop-shadow-lg bg-gray-100 drop-shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in-out duration-200">
+          <a onClick={handleNav} href="#work" className="w-[75%] flex justify-center items-center rounded-full drop-shadow-lg bg-gray-100 drop-shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in-out duration-200">
             <BsPerson size={20}/>
             <span className="pl-4">Expériences</span>
           </a>
-          <a href="#projet" className="w-[75%] flex justify-center items-center rounded-full drop-shadow-lg bg-gray-100 drop-shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in-out duration-200">
+          <a onClick={handleNav} href="#projet" className="w-[75%] flex justify-center items-center rounded-full drop-shadow-lg bg-gray-100 drop-shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in-out duration-200">
             <GrProjects size={20}/>
             <span className="pl-4">Projets</span>
           </a>
-          <a href="#contact" className="w-[75%] flex justify-center items-center rounded-full drop-shadow-lg bg-gray-100 drop-shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in-out duration-200">
+          <a onClick={handleNav} href="#contact" className="w-[75%] flex justify-center items-center rounded-full drop-shadow-lg bg-gray-100 drop-shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in-out duration-200">
             <AiOutlineMail size={20}/>
             <span className="pl-4">Contact</span>
           </a>
