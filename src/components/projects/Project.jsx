832367a8ac5b207df199966928debdf283d27524
Jayson_Delion <jayson-delion_student2023@wilder.school>
2023-08-10 13:59:18 +0200

    projects links fixed

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,14 +1,27 @@
 import PropTypes from "prop-types";
 
-const projectsURLS = ["https://github.com/Jayson-WCS/hidelidays-hackathon-1", "https://github.com/Jayson-WCS/projet-snake","https://github.com/Jayson-WCS/projet-omnifood", ""]
-
-export const Project = ( {img, title} ) => {
+export const Project = ( {img, title, repoLink, activeLink} ) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xs shadow-gray-400 rounded-xl group hover:bg-[#683790] transition delay-150 duration-300 ease-in-out">
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xs shadow-gray-400 rounded-xl group hover:bg-[#a568d7] transition delay-150 duration-300 ease-in-out">
       <img src={img} alt={title} className="rounded-xl group-hover:opacity-10" />
       <div className="hidden delay-300 group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
-        <h3 className="text-2xl text-bold mb-6 text-[#f1f5f9] tracking-wider text-center">{title}</h3>
-        <a href={projectsURLS} className="text-center px-4 py-1.5 m-auto rounded-lg bg-white/80 text-gray-700 font-bold cursor-pointer text-base">Code</a>
+        <h3 className="text-4xl font-bold mb-8 text-[#f1f5f9] text-center">{title}</h3>
+          <a
+          href={repoLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-center px-4 py-1.5 m-auto rounded-lg bg-white/80 text-gray-700 font-bold cursor-pointer text-base"
+        >
+          Code
+        </a>
+        {activeLink ? <a
+          href={activeLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-white/80 text-gray-800 font-bold ml-3 py-1.5 px-4 rounded-lg"
+        >
+          Visite
+        </a> : <span className="bg-white/80 text-gray-800 font-bold py-1.5 px-4 ml-3 rounded-lg opacity-50 cursor-not-allowed">À venir</span>}
       </div>
     </div>
   )
@@ -17,4 +30,6 @@ export const Project = ( {img, title} ) => {
 Project.propTypes = {
   img: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  repoLink: PropTypes.string.isRequired,
+  activeLink: PropTypes.string.isRequired,
 };
--- src/components/projects/ProjectsDisplay.jsx
+++ src/components/projects/ProjectsDisplay.jsx
@@ -5,15 +5,16 @@ import omnifood from "../../assets/omnifood.png";
 import snakeGame from "../../assets/snakeGame.png";
 
 export const ProjectsDisplay = () => {
+  
   return (
     <div id="projet" className="m-auto lg:pl-20 md:pl-28 p-4 py-20 bg-[#18181b] text-[#f1f5f9]">
       <h1 className="lg:pl-20 lg:pr-20 text-5xl mb-[36px] font-bold text-[#8b49c0]">Projets</h1>
       <p className="lg:pl-20 lg:pr-20 py-8 mb-6 text-left leading-10 text-xl font-light">Ces réalisations datent de cette année, certains sont des projets fait lors de ma formation à la Wild code School, d&apos;autres de sont issus de ma propre initiative.</p>
       <div className="grid lg:pl-20 lg:pr-20 sm:grid-cols-2 gap-16">
-        <Project img={omnifood} title="Omnifood" />
-        <Project img={makeSense} title="Make_Sense" />
-        <Project img={snakeGame} title="Snake" />
-        <Project img={hackathonAlibi} title="Hidelidays" />
+        <Project activeLink="https://jayson-wcs.github.io/projet-omnifood/" repoLink="https://github.com/Jayson-WCS/projet-omnifood" img={omnifood} title="Omnifood" />
+        <Project repoLink="https://github.com/Jayson-WCS/2023-02-JS-Bordeaux-project-3-groupe3/tree/dev" img={makeSense} title="Make_Sense" />
+        <Project activeLink="https://jayson-wcs.github.io/projet-snake/" repoLink="https://github.com/Jayson-WCS/projet-snake" img={snakeGame} title="Snake" />
+        <Project repoLink="https://github.com/Jayson-WCS/hidelidays-hackathon-1" img={hackathonAlibi} title="Hidelidays" />
       </div>
     </div>
   )
